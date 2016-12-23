4b7df6a1d5384f922eaaaf329ca2a1b4b505c761
ti39 <ti39@ti39.pioxii.com.br>
2016-12-23 09:06:06 -0200

    teste

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-
-public class BuscaEmpresa implements Tarefa {
-	
+public class BuscaEmpresa implements Tarefa {	
 	public BuscaEmpresa() {
 	    System.out.println("Instanciando uma Servlet do tipo BuscaEmpresa "
 	            + this);
